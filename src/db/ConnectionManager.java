c3bb647ed67533b11664c677be53d748286c0293
fadilahmuh <67127698+fadilahmuh@users.noreply.github.com>
2020-07-13 07:51:17 +0700

    test push

---
+++ src/db/ConnectionManager.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+/**
+ *
+ * @author fadil
+ */
+
+public class ConnectionManager {
+    private Connection con;
+    private String driver = "com.mysql.jdbc.Driver";
+    private String url = "jdbc:mysql://localhost:3306/rumahsakit";
+    private String username = "root";
+    private String password = "";
+    
+    public Connection logOn(){
+        try{
+            Class.forName(driver).newInstance();            
+            con = DriverManager.getConnection(url, username, password);
+        }
+        catch(Exception ex){
+            ex.printStackTrace();
+        }   
+            return con;
+    }
+        
+    public void logOff(){
+        try{
+            con.close();
+        }
+        catch(Exception ed){
+            ed.printStackTrace();
+        }
+    }
+}
