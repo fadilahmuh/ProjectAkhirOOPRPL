5752c72c95cb2d514f39f28df7fe082f9046201b
Erlangga Ardiansyah <61130170+erlanggaard@users.noreply.github.com>
2020-07-16 19:40:37 +0700

    Update Obat.java

--- src/com/Obat.java
+++ src/com/Obat.java
@@ -2,16 +2,16 @@ package com;
 public class Obat {
     String id_obat,nama_obat;
     int harga_obat;
-
+//construtor kosong
     public Obat() {
     }
-
+//construtor full
     public Obat(String id_obat, String nama_obat, int harga_obat) {
         this.id_obat = id_obat;
         this.nama_obat = nama_obat;
         this.harga_obat = harga_obat;
     }
-
+//getter and setter
     public String getId_obat() {
         return id_obat;
     }
@@ -35,7 +35,7 @@ public class Obat {
     public void setHarga_obat(int harga_obat) {
         this.harga_obat = harga_obat;
     }
-
+//toString
     @Override
     public String toString() {
         return "Obat{" + "id_obat=" + id_obat + ", nama_obat=" + nama_obat + ", harga_obat=" + harga_obat + '}';
